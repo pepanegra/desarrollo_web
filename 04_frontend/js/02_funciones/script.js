415a660bc2b5a988b990b74ca32c8a9e2885e09e
Jonathan_Angulo <jonathangabrielangulo95@gmail.com>
2023-06-24 12:12:45 -0500

    Estructura de carpetas

---
+++ 04_frontend/js/02_funciones/script.js
@@ -0,0 +1,23 @@
+function saludar(nombre) {
+  return "hola " + nombre;
+}
+
+console.log(saludar("pepito")); //1
+
+function sumar(valor1, valor2) {
+  return valor1 + valor2;
+}
+
+var totalcompra = sumar(valor1, valor2);
+
+console.log(sumar(5, 2));
+
+// Llamar una function
+sumar(4, 6);
+
+function restar(valor3, valor4) {
+  console.log(valor3 - valor4);
+}
+
+// Llamar una function
+restar(10, 6);
