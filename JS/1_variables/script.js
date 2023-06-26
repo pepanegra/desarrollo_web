9fa72a68c62e27b7089c55a3a520ec23a15050cb
Jonathan_Angulo <jonathangabrielangulo95@gmail.com>
2023-06-26 15:51:07 -0500

    js

---
+++ JS/1_variables/script.js
@@ -0,0 +1,26 @@
+/* var hola = "Hola Mundo";
+let hello = 'Hello World'
+console.log(hola);
+console.log(hello)*/
+
+let a;
+const pi = 3.1416;
+// const nueve;
+console.log(pi);
+// pi = 3.15
+
+a = "kenai";
+console.log(a);
+
+const objeto = {
+  nombre: "jonathan",
+  edad: 15,
+};
+
+const colores = [amarillo, azul, rojo];
+
+console.log(objeto);
+console.log(colores);
+
+objeto.correo = "jonathan@gmail.com";
+colores.push(green);
