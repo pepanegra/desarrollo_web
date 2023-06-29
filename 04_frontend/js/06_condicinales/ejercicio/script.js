8400a4422a70c1350fbdc21a0311b574ce625b9a
Jonathan_Angulo <jonathangabrielangulo95@gmail.com>
2023-06-29 17:27:09 -0500

    Trabajo del 19 de junio del 2023

---
+++ 04_frontend/js/06_condicinales/ejercicio/script.js
@@ -0,0 +1,37 @@
+function tryhard(numero) {
+  if (numero % 3 == 0 && numero % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (numero % 3 == 0) {
+    console.log("Fizz");
+  } else if (numero % 5 == 0) {
+    console.log("Buzz");
+  } else if (numero % 3 > 0 && numero % 5 > 0) {
+    console.log("El numero que ha ingresado no es multiplo del 3 ni del 5");
+  }
+}
+
+tryhard(false);
+
+function par(Number) {
+  if (Number % 2 == 0) {
+    console.log("El numero es par");
+  } else if (Number % 2 > 0) {
+    console.log("El numero es impar");
+  }
+}
+
+par(8);
+par(57);
+
+let w = prompt("Ingrese su peso");
+let h = prompt("Ingresa tu altura");
+let imt = w / h ** 2;
+if (imt < 18.5) {
+  console.log("Underweight");
+} else if (imt >= 18.5 && imt < 25) {
+  console.log("Normal");
+} else if (imt >= 25 && imt < 30) {
+  console.log("Overweight");
+} else {
+  console.log("Obesity");
+}
---
+++ 04_frontend/js/06_condicinales/script.js
@@ -0,0 +1,25 @@
+/*if (3 > 5) {
+  console.log("Tengo hambre");
+} else {
+  console.log("no tengo hambre");
+}*/
+
+// let edad = 19;
+
+// if (edad >= 18) {
+//   console.log("Crack eres un adulto");
+// } else {
+//   console.log("Eres menor de edad");
+// }
+
+let semaforo = "rojo";
+
+if (semaforo == "rojo") {
+  console.log("Debes detenerte");
+} else if (semaforo == "amarillo") {
+  console.log("Preparate para arrancar");
+} else if (semaforo == "verde") {
+  console.log("arrancar");
+} else {
+  console.log("solo se pueden los valores rojo, amarillo y verde");
+}
